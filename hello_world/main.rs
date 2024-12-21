88d38ffc8b4c0369b9ff781542eadce8a3b111d8
Taha-Sardar <taha.sardar@nixorcollege.edu.pk>
2024-12-21 20:17:27 +0500

    add hello world program

---
+++ hello_world/main.rs
@@ -0,0 +1,5 @@
+fn main(){
+    println!("Hello, World!"); // The ! calls a rust macro - not a function
+    // In order to compile the programme - rustc main.rs
+    // To run the programme - ./main
+}
